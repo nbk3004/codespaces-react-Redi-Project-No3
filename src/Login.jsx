593ddd9924dae243378822c6106e2705d1e91ee9
Nancy <115103244+nbk3004@users.noreply.github.com>
2024-12-16 16:54:42 +0000

    update

--- src/Login.jsx
+++ src/Login.jsx
@@ -16,7 +16,7 @@ const Login = ({ setUser }) => {
 
   return (
     <div className="login">
-      <h2>Welcome to Tweet App</h2>
+      <h2>Welcome to My Twitter App</h2>
       <input
         type="text"
         placeholder="Enter your name"
